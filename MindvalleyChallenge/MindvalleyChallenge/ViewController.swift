f8afbf6a8847af97460c615edcce0c6cacaede5a
Omar Samir <omarsamir@live.co.uk>
2018-09-14 13:35:20 +0200

    feat: inti commit
    mindvalley challenge init commit

---
+++ MindvalleyChallenge/MindvalleyChallenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MindvalleyChallenge
+//
+//  Created by Omar Samir on 9/14/18.
+//  Copyright © 2018 Omar Samir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
